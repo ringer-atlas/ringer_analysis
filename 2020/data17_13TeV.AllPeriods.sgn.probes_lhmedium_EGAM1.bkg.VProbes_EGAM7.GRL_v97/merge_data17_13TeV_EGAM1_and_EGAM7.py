06b8a8fed03a51f258cdc3468550fb92c48a2b44
jodafons <jodafons@lps.ufrj.br>
2021-06-27 17:11:27 -0300

    update all scripts

---
+++ 2020/data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97/merge_data17_13TeV_EGAM1_and_EGAM7.py
@@ -0,0 +1,10 @@
+
+
+from CollectorTool  import CreateData
+
+merge  = CreateData(60)
+
+merge( 'data17_EGAM1', 'data17_EGAM7', 'data17_13TeV.AllPeriods.sgn.probes_lhmedium_EGAM1.bkg.VProbes_EGAM7.GRL_v97' )
+
+
+
